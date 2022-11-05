72251da4523acca941f878da230612e924458772
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-11-05 19:56:36 +0600

    Реализовал просмотр списка задач

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from webapp.models import Task
 
-# Create your views here.
+
+def index_view(request):
+    tasks = Task.objects.all()
+    return render(request, 'index.html', {'tacks': tasks})
