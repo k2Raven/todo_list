68190d67dd3d44e1e3e4e4080df6ced2bd46edba
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-11-05 19:38:33 +0600

    Создал модель Task, создал и применил миграции, зарегистрировал модель в админке

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webapp.models import Task
 
-# Register your models here.
+
+class TackAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'status', 'deadline']
+    list_filter = ['status']
+    search_fields = ['title']
+    exclude = []
+
+
+admin.site.register(Task, TackAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
+
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=60, verbose_name='Название')
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0],
+                              verbose_name='Статус')
+    deadline = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return f'{self.id}. {self.title}'
