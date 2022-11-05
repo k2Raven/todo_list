b96137b8090fe92a9ca04e8a61b57717a44e2c8e
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-11-05 20:24:14 +0600

    Реализовал создание новой задачи

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from webapp.models import Task
+from webapp.models import Task, STATUS_CHOICES
 
 
 def index_view(request):
     tasks = Task.objects.all()
     return render(request, 'index.html', {'tacks': tasks})
+
+
+def create_task(request):
+    if request.method == "GET":
+        return render(request, 'create.html', {'statuses': STATUS_CHOICES})
+    elif request.method == "POST":
+        title = request.POST.get('title')
+        status = request.POST.get('status')
+        deadline = request.POST.get('deadline')
+        print(deadline)
+        new_task = Task.objects.create(title=title, status=status, deadline=deadline)
+        return render(request, 'task_view.html', {'task': new_task})
