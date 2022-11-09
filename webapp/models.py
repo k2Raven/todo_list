4f54377693077f3fb28d7c78c4e9b144c6f06fd6
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-11-09 19:29:55 +0600

    В модель Task, добавил поле description, создал и применил миграцию

--- webapp/models.py
+++ webapp/models.py
@@ -9,6 +9,7 @@ class Task(models.Model):
     status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0],
                               verbose_name='Статус')
     deadline = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
 
     def __str__(self):
         return f'{self.id}. {self.title}'
