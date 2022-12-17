d2192a08aee1003eaf53bca5b2f4bc7e72313319
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-12-17 19:50:57 +0600

    Выполнил дз 53 и 54

--- webapp/views/project_views.py
+++ webapp/views/project_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.urls import reverse
+from django.views.generic.list import MultipleObjectMixin
 
 from webapp.models import Project
 from django.views.generic import ListView, DetailView, CreateView
@@ -11,19 +12,14 @@ class ProjectListView(ListView):
     context_object_name = 'projects'
     ordering = '-date_started'
 
-class ProjectDetail(DetailView):
+class ProjectDetail(DetailView, MultipleObjectMixin):
     model = Project
     template_name = 'project/project_view.html'
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         tasks = self.object.tasks.all()
-        paginator = Paginator(tasks, 2)
-        page_number = self.request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-        context = super().get_context_data(**kwargs)
-        context['page_obj'] = page_obj
-        context['is_paginated'] = page_obj.has_other_pages()
-        context['tasks'] = page_obj.object_list
+        context = super().get_context_data(object_list=tasks, **kwargs)
         return context
 
 class ProjectCreate(CreateView):
